735035bf47329219ff6da7c910ddc522156839dc
hyunah-park <58015130+hyunah-park@users.noreply.github.com>
2019-11-21 11:20:32 +0900

    Create C_project02.c

---
+++ C_project02.c
@@ -0,0 +1,16 @@
+# 2017-04-04
+# 사용자로부터 x의 값을 실수로 입력받아서 다음과 같은 다항식의 값을 계산하는 프로그램을 작성하여 보자.
+# [다음] 3x^3-7x^2+9
+
+#include <stdio.h>
+int main()
+{
+	double x, result;
+
+	printf("x의 값을 입력하십시오: ");
+	scanf_s("%lf", &x);
+
+	result = 3*x*x*x - 7*x*x + 9;
+	printf("다항식의 값은 %lf입니다. \n", result);
+	return 0;
+}
