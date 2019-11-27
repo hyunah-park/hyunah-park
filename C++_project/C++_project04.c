2b243074f06cc6d54d22ef3abe7208225b45c2fd
hyunah-park <58015130+hyunah-park@users.noreply.github.com>
2019-11-27 13:17:46 +0900

    Create C++_project04.c

---
+++ C++_project/C++_project04.c
@@ -0,0 +1,20 @@
+/*
+2017-09-12
+다음과 같이 출력하는 C++ 프로그램을 Visual C++로 작성하라.
+
+[다음]
+*
+**
+***
+****
+
+*/
+
+#include <iostream>
+int main() {
+std::cout << "*\n";
+std::cout << "**\n";
+std::cout << "***\n";
+std::cout << "****";
+return 0;
+}
