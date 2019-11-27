782b13290a2f0e5b227b2334300ce491760b33e1
hyunah-park <58015130+hyunah-park@users.noreply.github.com>
2019-11-27 13:15:31 +0900

    Create C++_project02.c

---
+++ C++_project/C++_project02.c
@@ -0,0 +1,12 @@
+/*
+2017-09-12
+자신의 학과, 나이, 희망 직업을 화면에 출력하는 C++ 프로그램을 Visual C++로 작성하라.
+*/
+
+#include <iostream>
+int main() {
+std::cout << "컴퓨터공학과\n";
+std::cout << "21세\n";
+std::cout << "대통령";
+return 0;
+}
